4046a95f3d1bb7a77379de9b8bf6b52f4a1f0354
Gim128 <gimhanpabasara4@gmail.com>
2022-02-14 01:18:12 +0530

    setup travelAppUI

---
+++ Travel_App_ui/src/consts/color.js
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+const COLOR = {
+  white: '#FFF',
+  dark: '#000',
+  primary: '#04555c',
+  secondary: '#f9f9f9',
+  grey: '#dddedd',
+  red: 'red',
+  orange: '#f5a623',
+};
+
+export default COLOR;
---
+++ Travel_App_ui/src/screens/DetailsScreen.js
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+
+const DetailsScreen = () => {
+  return <SafeAreaView></SafeAreaView>;
+};
+
+const style = StyleSheet.create({});
+
+export default DetailsScreen;
