759ebd3445ef52009772426f7ea99c7b6662aa21
Gim128 <gimhanpabasara4@gmail.com>
2022-02-15 15:23:04 +0530

    start to design home screen

--- Travel_App_ui/src/screens/HomeScreen.js
+++ Travel_App_ui/src/screens/HomeScreen.js
@@ -1,11 +1,30 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {SafeAreaView, ScrollView, StatusBar, StyleSheet, View} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import COLOR from '../consts/color';
 
 const HomeScreen = ({navigation}) => {
-  return <SafeAreaView></SafeAreaView>;
+  return <SafeAreaView
+  style={{flex:1, backgroundColor: COLOR.white}}>
+    <StatusBar translucent = {false} backgroundColor = {COLOR.primary}/>
+    <View style = {style.header}>
+      <Icon name="sort" size={28} color = {COLOR.white} />
+      <Icon name="notifications-none" size={28} color = {COLOR.white} />
+    </View>
+    <ScrollView showsVerticalScrollIndicator={false}></ScrollView> 
+    {/* 15.28 */}
+  </SafeAreaView>;
 };
 
-const style = StyleSheet.create({});
+const style = StyleSheet.create({
+  header : { 
+    paddingVertical : 20,
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: COLOR.primary,
+  },
+});
 
 export default HomeScreen;
--- Travel_App_ui/src/screens/OnBoardScreen.js
+++ Travel_App_ui/src/screens/OnBoardScreen.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unreachable */
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import {ImageBackground, SafeAreaView, StatusBar, StyleSheet, View} from 'react-native';
+import {ImageBackground, SafeAreaView, StatusBar, StyleSheet, View, Text, TouchableOpacity} from 'react-native';
 import COLOR from '../consts/color';
 
 const OnBoardScreen = ({navigation}) => {
@@ -17,6 +17,13 @@ const OnBoardScreen = ({navigation}) => {
             <Text style={{color: COLOR.white, fontSize: 35, fontWeight: 'bold'}}>World With Us</Text>
 
             <Text style={{color: COLOR.white, lineHeight: 25, marginTop: 15}}>lorem ipsum ffrrttnfkvjnffvjn ,cm v.jfbvujvbkjdb.JDNkjdk.jbkjvbfljbv</Text>
+           
+            <TouchableOpacity activeOpacity={0.8} onPress={() => navigation.navigate("HomeScreen")}>
+                <View style={style.btn}>
+                    <Text style={{fontWeight: 'bold'}}>GET START</Text>
+                </View>
+            </TouchableOpacity>
+           
         </View>
 
         
@@ -26,11 +33,22 @@ const OnBoardScreen = ({navigation}) => {
 
 const style = StyleSheet.create({
     details: {
-        height: 50%,
+        height: '50%',
         bottom: 0,
         position: 'absolute',
         paddingHorizontal: 40,
-    }
+    },
+
+    btn: {
+        height: 50,
+        width: 120,
+        backgroundColor: COLOR.white,
+        marginTop: 20,
+        borderRadius: 7,
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    },
 });
 
 export default OnBoardScreen;
